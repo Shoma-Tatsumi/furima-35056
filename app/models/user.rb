1a44a0fdc2e36974cc5c1dd3477b57c025ba697b
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-03-28 23:07:27 +0900

    passwordのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :password, format: {with: /(?=.*[a-z])(?=.*\d)[a-z\d]/i, message: "Include both letters and numbers."}
 
   with_options presence: true do
     validates :nickname
