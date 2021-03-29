a1466cacf8cf41872bb52b94ae82c5c4050d6dc1
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-03-29 16:41:12 +0900

    Revert "Rubocopにて修正"
    This reverts commit cfc6fb304f7526d45593c337c3af7d5a6c92161b.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :password, format: { with: /(?=.*[a-z])(?=.*\d)[a-z\d]/i, message: 'Include both letters and numbers.' }
+  
+  validates :password, format: {with: /(?=.*[a-z])(?=.*\d)[a-z\d]/i, message: "Include both letters and numbers."}
 
   with_options presence: true do
     validates :nickname
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々 ー]+ \z/, message: 'Full-width characters.' }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々 ー]+ \z/, message: 'Full-width characters.' }
-    validates :first_name_reading, format: { with: /\A[ァ-ヶー]+ \z/, message: 'Full-width katakana characters.' }
-    validates :last_name_reading, format: { with: /\A[ァ-ヶー]+ \z/, message: 'Full-width katakana characters.' }
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ , message: "Full-width characters." }
+    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ , message: "Full-width characters." }
+    validates :first_name_reading, format: {with: /\A[ァ-ヶー]+\z/ , message: "Full-width katakana characters." }
+    validates :last_name_reading, format: {with: /\A[ァ-ヶー]+\z/ , message: "Full-width katakana characters." }
     validates :birthday
   end
 end
