9823d877eaa24040039d73d8436a4db7be4e2745
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-03-30 21:21:48 +0900

    jsを用いた販売手数料と販売利益の表示

---
+++ app/javascript/item_price.js
@@ -0,0 +1,12 @@
+function item_price (){
+  const priceInput = document.getElementById("item-price");
+  const addTax = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    addTax.innerHTML = Math.floor(inputValue * 0.1);
+    profit.innerHTML = inputValue - Math.floor(inputValue * 0.1);
+  });
+}
+
+window.addEventListener('load', item_price)
