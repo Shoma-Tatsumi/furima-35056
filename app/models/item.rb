83fb05c1454361934c4ef90dd2c764ab7e6b2a68
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-03-30 12:10:23 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,25 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :days_to_ship
+
+  with_options presence: true do
+    validates :image
+    validates :title
+    validates :description
+    validates :price
+    with_options numericality: {other_than: 0, message: "can't be blank"} do
+      validates :category_id
+      validates :condition_id
+      validates :delivery_fee_id
+      validates :prefecture_id
+      validates :days_to_ship_id
+    end
+  end
 end
