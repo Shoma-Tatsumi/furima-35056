d9d07f93e3425272c1002cffb47f0faf01b30676
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-03-30 16:45:43 +0900

    出品した情報をテーブルに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,5 +7,16 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :title, :description, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
 end
