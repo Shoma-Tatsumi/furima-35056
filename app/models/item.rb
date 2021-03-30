c1523cf3d8c6e14f39688d5595ecb2d32a3ee9df
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-03-30 18:24:08 +0900

    Itemモデルテストコード作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,9 +16,9 @@ class Item < ApplicationRecord
     validates :price
   end
 
-  validates :price, format: {with: /\A[0-9]+\z/, message: "Half-width number"}
-  validates :price, format: {with: /[3-9][0-9]{2}|[1-9][0-9]{3,6}/, message: "Out of setting range"} 
-
+  validates :price, numericality: { only_integer: true, message: "Half-width number"}
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range" }
+  
   with_options numericality: {other_than: 0, message: "Select"} do
     validates :category_id
     validates :condition_id
@@ -26,5 +26,4 @@ class Item < ApplicationRecord
     validates :prefecture_id
     validates :days_to_ship_id
   end
-  
 end
