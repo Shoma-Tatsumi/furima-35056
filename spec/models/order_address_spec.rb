21374b9ce908fe36b2ad5c3862fad263d11b3307
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-04-01 23:47:37 +0900

    Formオブジェクトテストコード実施

---
+++ spec/models/order_address_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+RSpec.describe OrderAddress, type: :model do
+  describe '購入情報の保存' do
+    before do
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
+    end
+
+    context '内容に問題ない場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@order_address).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'postal_codeが空だと保存できないこと' do
+        @order_address.postal_code = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it 'postal_codeがハイフンを含んだ正しい形式でないと保存できないこと' do
+        @order_address.postal_code = '1234567'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code Input correctly")
+      end
+      it 'prefecture_idを選択していないと保存できないこと' do
+        @order_address.prefecture_id = 0
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Prefecture Select")
+      end
+      it 'cityが空だと保存できないこと' do
+        @order_address.city = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("City can't be blank")
+      end
+      it 'house_numberが空だと保存できないこと' do
+        @order_address.house_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("House number can't be blank")
+      end
+      it 'building_numberは空でも保存できること' do
+        @order_address.building_number = ''
+        expect(@order_address).to be_valid
+      end
+      it 'phone_numberが空だと保存できないこと' do
+        @order_address.phone_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it 'phone_numberが全角数字だと保存できないこと' do
+        @order_address.phone_number = '１２３４５６７８９'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number Input only number")
+      end
+      it 'phone_numberが英数混合だと保存できないこと' do
+        @order_address.phone_number = '12345abcde'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number Input only number")
+      end
+      it 'phone_numberが半角英語だと保存できないこと' do
+        @order_address.phone_number = 'abcdefghij'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number Input only number")
+      end
+    end
+  end
+end
