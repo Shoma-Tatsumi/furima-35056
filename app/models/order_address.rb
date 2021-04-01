db71386cd50b662a14d790f258a56e5a2d1a5751
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-04-01 15:58:16 +0900

    Formオブジェクト作成

---
+++ app/models/order_address.rb
@@ -0,0 +1,19 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_number, :phone_number, :item_id, :user_id
+
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly" }
+    validates :prefecture_id, numericality: {other_than: 0, message: "Select"}
+    validates :city
+    validates :house_number
+    validates :phone_number, numericality: { only_integer: true, max_length: 11, message: "Input only number"}
+    validates :item_id
+    validates :user_id
+  end
+
+  def save
+    order = Order.create(item_id: @item_id, user_id: @user_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_number: building_number, phone_number: phone_number, order_id: order.id)
+  end
+end
