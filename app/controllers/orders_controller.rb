bbf020d4fa12df8b07f9ccdd0cf424fbff8f65a4
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-04-01 23:47:57 +0900

    リファクタリング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,9 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: :index
+  before_action :set_item, only: [:index, :create]
+  before_action :set_redirect, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
   def create
@@ -9,13 +12,21 @@ class OrdersController < ApplicationController
       @order_address.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render :index
     end
   end
 
   private
+  
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_number, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def set_redirect
+    redirect_to root_path if current_user.id == @item.user_id
   end
 end
