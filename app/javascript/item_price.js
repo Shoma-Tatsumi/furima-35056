01d30a40003ce8da8249daa46b277b259e0e3a36
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-04-04 10:21:56 +0900

    決済機能の実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,11 +2,11 @@ function item_price (){
   const priceInput = document.getElementById("item-price");
   const addTax = document.getElementById("add-tax-price");
   const profit = document.getElementById("profit");
-  priceInput.addEventListener("input", () => {
+  priceInput.addEventListener('input', () => {
     const inputValue = priceInput.value;
     addTax.innerHTML = Math.floor(inputValue * 0.1);
     profit.innerHTML = inputValue - Math.floor(inputValue * 0.1);
   });
-}
+};
 
-window.addEventListener('load', item_price)
+window.addEventListener('load', item_price);
