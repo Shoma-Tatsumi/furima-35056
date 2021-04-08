3516d1da7059c2c95f0a48d9d140ea4d9dc054a5
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-04-08 14:45:43 +0900

    修正

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe OrderAddress, type: :model do
     before do
       user = FactoryBot.create(:user)
       item = FactoryBot.create(:item)
-      sleep 0.1
+      # sleep 0.1
       @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
     end
 
